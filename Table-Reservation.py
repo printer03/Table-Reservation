032e05f11e8060829f52f738b8ba6a06157894c6
printer03 <s7070017@kmitl.ac.th>
2014-11-19 17:03:22 +0700

    Add codes

--- Table-Reservation.py
+++ Table-Reservation.py
@@ -2,22 +2,37 @@ from Tkinter import *
 class Check_in(object):
     root = Tk()
     box = Entry(root, width = 30)
-    def __init__(self):
+    root1 = Tk()
+    
+    def __init__(self, count = 0):
         head = Label(self.root, text="Name")
         head.pack()
         self.box.pack()
-        self.b = Button(self.root, text="Reserve", width=10, command=self.callback)
+        self.b = Button(self.root, text="Reserve", width=10\
+                        , command=self.monitor)
         self.b.pack()
         self.root.mainloop()
-        
-    def callback(self):
+
+    def monitor(self):
         self.name = self.box.get()
+        head1 = Label(self.root1, text=self.name)
+        head1.pack()
+        self.b = Button(self.root1, text="Order", width=10\
+                        , command=self.order_food)
+        self.b.pack()
         print self.name
         
-##class monitor(object):
-##    root = Tk()
-##    box = Entry(root, width = 30)
-##    def __init__(self):
-        
-    
+    def order_food(self):
+        root2 = Tk()
+        self.b = Button(root2, text="Noodle", width=10\
+                        , command=self.monitor)
+        self.b.pack()
+        self.b = Button(root2, text="Fried chicken", width=10\
+                        , command=self.monitor)
+        self.b.pack()
+        self.b = Button(root2, text="Rice", width=10\
+                        , command=self.monitor)
+        self.b.pack()
+
 Check_in()
+
