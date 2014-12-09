8dfa95741632cc3857b141ef0767e7e6b65f6c5b
printer03 <s7070017@kmitl.ac.th>
2014-12-10 00:35:10 +0700

    Edited 10-12-2014

--- Table-Reservation.py
+++ Table-Reservation.py
@@ -18,13 +18,16 @@ class Check_in(object):
     count = 300
     data1, data2, data3, data4, data5 = [], [], [], [], []
     total1, total2, total3, total4, total5 = [], [], [], [], []
+    order_amount = dict()
     
     def __init__(self):
         price = []
+        about = Button(self.root, width = 115, text="About this program", command=self.detail)
+        about.place(x=0, y=0)
         head = Label(self.root, text="Welcome to 5 Tables Restaurant", bg='#9F000F')
-        head.place(x=320, y=20)
-        head1 = Label(self.root, text="Name", bg='#9F000F')
-        head1.place(x=385, y=50)
+        head.place(x=320, y=40)
+        head1 = Label(self.root, text="Enter your name here", bg='#9F000F')
+        head1.place(x=340, y=70)
         self.box.place(x=250, y=100)
         self.b = Button(self.root, text="Reserve", width=10\
                         , command=self.monitor, bg = '#FDD017')
@@ -32,6 +35,7 @@ class Check_in(object):
         self.root.mainloop()
 
     def monitor(self):
+        '''This function is use to link all button and another option'''
         if self.count >= 500:
             root1 = Tk()
             caution = Label(root1, text="Table are full reserved now, please checking again in 10 minutes")
@@ -58,8 +62,11 @@ class Check_in(object):
                     s = OptionMenu(self.root, self.spindle, *list1)
                     s.place(x=250, y=self.count)
 
-                    button = Button(self.root, text="Submit", command=self.submit, bg = '#FFDB58')
-                    button.place(x=400, y=self.count)
+                    button_submit = Button(self.root, text="Submit", command=self.submit, bg = '#FFDB58')
+                    button_submit.place(x=400, y=self.count)
+
+                    button_calcel = Button(self.root, text="Cancel", command=self.cancel, bg = '#FFDB58')
+                    button_calcel.place(x=450, y=self.count)
 
                     menu_list = Button(self.root, text="Your Ordered", command=self.menu_list, bg = '#FFD801')
                     menu_list.place(x=550, y=self.count)
@@ -86,69 +93,144 @@ class Check_in(object):
                     break
 
     def submit(self):
+        '''This function is use to command to save list of menu that customer ordered to this restaurant'''
         print self.spindle.get()
         ##add price to data##
+        if self.spindle.get() not in self.order_amount:
+            self.order_amount[self.spindle.get()] = 1
+        else:
+            self.order_amount[self.spindle.get()] += 1
+        if self.spindle.get() != 'Pre-Order':
+            if self.count == 340:
+                self.data1.append(self.spindle.get())
+                self.total1.append(int(self.spindle.get()[-1]))
+            elif self.count == 380:
+                self.data2.append(self.spindle.get())
+                self.total2.append(int(self.spindle.get()[-1]))
+            elif self.count == 420:
+                self.data3.append(self.spindle.get())
+                self.total3.append(int(self.spindle.get()[-1]))
+            elif self.count == 460:
+                self.data4.append(self.spindle.get())
+                self.total4.append(int(self.spindle.get()[-1]))
+            elif self.count == 500:
+                self.data5.append(self.spindle.get())
+                self.total5.append(int(self.spindle.get()[-1]))
+
+    def cancel(self):
+        '''This function is use to cancel things that customer change there mind'''
+        print self.spindle.get()
         if self.count == 340:
-            self.data1.append(self.spindle.get())
-            self.total1.append(int(self.spindle.get()[-1]))
+            self.order_amount[self.spindle.get()] -= 1
+            if self.order_amount[self.spindle.get()] == 0:
+                self.data1.remove(self.spindle.get())
+            self.total1.remove(int(self.spindle.get()[-1])) ##remove price in total1
         elif self.count == 380:
-            self.data2.append(self.spindle.get())
-            self.total2.append(int(self.spindle.get()[-1]))
+            self.order_amount[self.spindle.get()] -= 1
+            if self.order_amount[self.spindle.get()] < 0:
+                self.data2.remove(self.spindle.get())
+                self.total2.remove(int(self.spindle.get()[-1]))
+            self.total1.remove(int(self.spindle.get()[-1])) ##remove price in total2
         elif self.count == 420:
-            self.data3.append(self.spindle.get())
-            self.total3.append(int(self.spindle.get()[-1]))
+            self.order_amount[self.spindle.get()] -= 1
+            if self.order_amount[self.spindle.get()] <= 0:
+                self.data3.remove(self.spindle.get())
+            self.total3.remove(int(self.spindle.get()[-1])) ##remove price in total3
         elif self.count == 460:
-            self.data4.append(self.spindle.get())
-            self.total4.append(int(self.spindle.get()[-1]))
+            self.order_amount[self.spindle.get()] -= 1
+            if self.order_amount[self.spindle.get()] <= 0:
+                self.data4.remove(self.spindle.get())
+            self.total4.remove(int(self.spindle.get()[-1])) ##remove price in total4
         elif self.count == 500:
-            self.data5.append(self.spindle.get())
-            self.total5.append(int(self.spindle.get()[-1]))
+            self.order_amount[self.spindle.get()] -= 1
+            if self.order_amount[self.spindle.get()] <= 0:
+                self.data5.remove(self.spindle.get())
+            self.total5.remove(int(self.spindle.get()[-1])) ##remove price in total5
         
     def menu_list(self):
+        '''This function is use to save list of menu that customer ordered'''
+        line = 0
         rootm = Tk()
+        Label(rootm, text='Amount').place(x=230, y=line)
+        self.data1 = list(set(self.data1))
+        self.data2 = list(set(self.data2))
+        self.data3 = list(set(self.data3))
+        self.data4 = list(set(self.data4))
+        self.data5 = list(set(self.data5))
         if self.count == 340:
             for i in self.data1:
-                Label(rootm, text=i).pack()
+                line += 30
+                Label(rootm, text=i).place(x=10, y=line)
+                Label(rootm, text=self.order_amount[i]).place(x=250, y=line)
         elif self.count == 380:
             for i in self.data2:
                 Label(rootm, text=i).pack()
+                line += 30
+                Label(rootm, text=i).place(x=10, y=line)
+                Label(rootm, text=self.order_amount[i]).place(x=250, y=line)
         elif self.count == 420:
             for i in self.data3:
                 Label(rootm, text=i).pack()
+                line += 30
+                Label(rootm, text=i).place(x=10, y=line)
+                Label(rootm, text=self.order_amount[i]).place(x=250, y=line)
         elif self.count == 460:
             for i in self.data4:
                 Label(rootm, text=i).pack()
+                line += 30
+                Label(rootm, text=i).place(x=10, y=line)
+                Label(rootm, text=self.order_amount[i]).place(x=250, y=line)
         elif self.count == 500:
             for i in self.data5:
                 Label(rootm, text=i).pack()
+                line += 30
+                Label(rootm, text=i).place(x=10, y=line)
+                Label(rootm, text=self.order_amount[i]).place(x=250, y=line)
+        rootm.geometry(str(300)+'x'+str(line+60))
         exits = Button(rootm, text='OK', command = rootm.destroy, bg = '#FDD017')
-        exits.pack()
+        exits.place(x=140, y=line+30)
 
     ####################################apart all button check bill for 5 customers####################################
     def check_bill1(self):
+        '''Keep database of Table1 and ready to calculate all the time'''
         rootb = Tk()
+        line = 0
+        Label(rootb, text='Amount').place(x=230, y=line)
+        self.data1 = list(set(self.data1))
         for i in self.data1:
-            Label(rootb, text=i).pack()
-        Label(rootb, text='Price = $' + str(sum(self.total1))).pack()
+            line += 30
+            Label(rootb, text=i).place(x=10, y=line)
+            Label(rootb, text=self.order_amount[i]).place(x=250, y=line)
+        Label(rootb, text='Price   =   $' + str(sum(self.total1))).place(x=200, y=line+30)
+        rootb.geometry(str(300)+'x'+str(line+90))
         exits = Button(rootb, text='OK', command = rootb.destroy, bg = '#FDD017')
-        exits.pack()
+        exits.place(x=140, y=line+60)
         a = Label(self.root, text='                '*100, bg='#9F000F')
         a.place(y=300)
         a = Label(self.root, text='                '*100, bg='#9F000F')
         a.place(y=310)
         self.listm.remove(300)
         self.count -= 40
+        print self.total1
         self.data1, self.total1 = [], []
+        self.order_amount = dict()
         print sum(self.total1)
         print self.listm
 
     def check_bill2(self):
+        '''Keep database of Table2 and ready to calculate all the time'''
         rootb = Tk()
+        line = 0
+        Label(rootb, text='Amount').place(x=230, y=line)
+        self.data2 = list(set(self.data2))
         for i in self.data2:
-            Label(rootb, text=i).pack()
-        Label(rootb, text='Price = $' + str(sum(self.total2))).pack()
+            line += 30
+            Label(rootb, text=i).place(x=10, y=line)
+            Label(rootb, text=self.order_amount[i]).place(x=250, y=line)
+        Label(rootb, text='Price   =   $' + str(sum(self.total2))).place(x=10, y=line+30)
+        rootb.geometry(str(300)+'x'+str(line+90))
         exits = Button(rootb, text='OK', command = rootb.destroy, bg = '#FDD017')
-        exits.pack()
+        exits.place(x=140, y=line+60)
         a = Label(self.root, text='                '*100, bg='#9F000F')
         a.place(y=340)
         a = Label(self.root, text='                '*100, bg='#9F000F')
@@ -156,16 +238,24 @@ class Check_in(object):
         self.listm.remove(340)
         self.count -= 40
         self.data2, self.total2 = [], []
+        self.order_amount = dict()
         print sum(self.total2)
         print self.listm
 
     def check_bill3(self):
+        '''Keep database of Table3 and ready to calculate all the time'''
         rootb = Tk()
+        line = 0
+        Label(rootb, text='Amount').place(x=230, y=line)
+        self.data3 = list(set(self.data3))
         for i in self.data3:
-            Label(rootb, text=i).pack()
-        Label(rootb, text='Price = $' + str(sum(self.total3))).pack()
+            line += 30
+            Label(rootb, text=i).place(x=10, y=line)
+            Label(rootb, text=self.order_amount[i]).place(x=250, y=line)
+        Label(rootb, text='Price   =   $' + str(sum(self.total3))).place(x=10, y=line+30)
+        rootb.geometry(str(300)+'x'+str(line+90))
         exits = Button(rootb, text='OK', command = rootb.destroy, bg = '#FDD017')
-        exits.pack()
+        exits.place(x=140, y=line+60)
         a = Label(self.root, text='                '*100, bg='#9F000F')
         a.place(y=380)
         a = Label(self.root, text='                '*100, bg='#9F000F')
@@ -173,16 +263,24 @@ class Check_in(object):
         self.listm.remove(380)
         self.count -= 40
         self.data3, self.total3 = [], []
+        self.order_amount = dict()
         print sum(self.total3)
         print self.listm
 
     def check_bill4(self):
+        '''Keep database of Table4 and ready to calculate all the time'''
         rootb = Tk()
+        line = 0
+        Label(rootb, text='Amount').place(x=230, y=line)
+        self.data4 = list(set(self.data4))
         for i in self.data4:
-            Label(rootb, text=i).pack()
-        Label(rootb, text='Price = $' + str(sum(self.total4))).pack()
+            line += 30
+            Label(rootb, text=i).place(x=10, y=line)
+            Label(rootb, text=self.order_amount[i]).place(x=250, y=line)
+        Label(rootb, text='Price   =   $' + str(sum(self.total4))).place(x=10, y=line+30)
+        rootb.geometry(str(300)+'x'+str(line+90))
         exits = Button(rootb, text='OK', command = rootb.destroy, bg = '#FDD017')
-        exits.pack()
+        exits.place(x=140, y=line+60)
         a = Label(self.root, text='                '*100, bg='#9F000F')
         a.place(y=420)
         a = Label(self.root, text='                '*100, bg='#9F000F')
@@ -190,16 +288,24 @@ class Check_in(object):
         self.listm.remove(420)
         self.count -= 40
         self.data4, self.total4 = [], []
+        self.order_amount = dict()
         print sum(self.total4)
         print self.listm
 
     def check_bill5(self):
+        '''Keep database of Table4 and ready to calculate all the time'''
         rootb = Tk()
+        line = 0
+        Label(rootb, text='Amount').place(x=230, y=line)
+        self.data5 = list(set(self.data5))
         for i in self.data5:
-            Label(rootb, text=i).pack()
-        Label(rootb, text='Price = $' + str(sum(self.total5))).pack()
+            line += 30
+            Label(rootb, text=i).place(x=10, y=line)
+            Label(rootb, text=self.order_amount[i]).place(x=250, y=line)
+        Label(rootb, text='Price   =   $' + str(sum(self.total5))).place(x=10, y=line+30)
+        rootb.geometry(str(300)+'x'+str(line+90))
         exits = Button(rootb, text='OK', command = rootb.destroy, bg = '#FDD017')
-        exits.pack()
+        exits.place(x=140, y=line+60)
         a = Label(self.root, text='                '*100, bg='#9F000F')
         a.place(y=460)
         a = Label(self.root, text='                '*100, bg='#9F000F')
@@ -207,7 +313,26 @@ class Check_in(object):
         self.listm.remove(460)
         self.count -= 40
         self.data5, self.total5 = [], []
+        self.order_amount = dict()
         print sum(self.total5)
         print self.listm
+
+    def detail(self):
+        rootd = Tk()
+        scrollb = Scrollbar(rootd)
+        texture = Text(rootd, height=4, width=57)
+        scrollb.pack(side=RIGHT, fill=Y)
+        texture.pack(side=LEFT, fill=Y)
+        scrollb.config(command=texture.yview)
+        texture.config(yscrollcommand=scrollb.set)
+        quote = """This program is use to reserve a table from another place. \
+You can pre-order for more rapid and comfort in your dialy life. \
+This program is help you that you haven't wait for food anymore \
+if you pre-order to there restaurant, all foods that will come \
+to your table in 5 minutes because we had cooked for you and boil \
+all the time. When you ate already you can click check bill button. \
+This program will calculate all price for you to pay"""
+        texture.insert(END, quote)
+        
         
 Check_in()
